19be04b9f4fbfb55b6bbe51c411bc38471336512
Cliff Berinti <cliff.berinti@gmail.com>
2014-12-23 18:15:35 -0600

    2. Collections

--- movies.js
+++ movies.js
@@ -1,11 +1,32 @@
+// Collection to store movies to be displayed
+Movies = new Mongo.Collection("movies");
+
+// Code that only runs on the client
 if (Meteor.isClient) {
+
+	// Helper functions
 	Template.body.helpers({
-		movies: [
-			{ text: "Teenage Mutant Ninja Turtles" } ,
-			{ text: "The Maze Runner" } ,
-			{ text: "The Skeleton Twins" }
-		]
+		movies: function() {
+			return Movies.find({}, {sort: {createAt: -1}}); // Sort most recent at the top
+		}
 	});
+
+	// Event functions
+	Template.body.events({
+		// Called when we submit a new movie
+		"submit .new-movie": function (event) {
+			var title = event.target.text.value;
+			// Insert movie into Mongo
+			Movies.insert({
+				title: title,
+				createAt: new Date() // current time
+			});
+			// Clear form
+			event.target.text.value = "";
+			// Prevent default form submit
+			return false;
+		}
+	})
 }
 
 if (Meteor.isServer) {
